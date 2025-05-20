531597ed5136d2707d32f168708ce25cc40eda08
Cristian Suarez <al21020012@itsa.edu.mx>
2025-05-20 14:59:26 -0600

    Fix puerto para Render y ajustes finales

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.delete('/inventory/:id', async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
